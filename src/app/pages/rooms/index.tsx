0001e345adb81c5d0e7851f94d34858fcf1f248c
Reuben Mark Gorda <reuben.gorda@gmail.com>
2026-03-13 19:01:11 +0800

    add basic carousel to room page

--- src/app/pages/rooms/index.tsx
+++ src/app/pages/rooms/index.tsx
@@ -1,6 +1,61 @@
-import { Container, Stack, Typography } from "@mui/material";
+import { Button, Container, Stack, Typography } from "@mui/material";
+import { useNavigate, useParams } from "react-router";
+import KeyboardBackspaceOutlinedIcon from "@mui/icons-material/KeyboardBackspaceOutlined";
+
+import type { EmblaOptionsType } from "embla-carousel";
+import ClassNames from "embla-carousel-class-names";
+import useEmblaCarousel from "embla-carousel-react";
+
+type PropType = {
+  slides: number[];
+  options?: EmblaOptionsType;
+};
+
+const OPTIONS: EmblaOptionsType = { loop: true };
+const SLIDE_COUNT = 5;
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys());
+
+const EmblaCarousel = (props: PropType) => {
+  const { slides, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [ClassNames()]);
+
+  return (
+    <section
+      style={{
+        backgroundColor: "red",
+        width: "100%",
+      }}
+      className="embla"
+    >
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div
+              className="embla__slide"
+              style={{
+                height: "70svh",
+              }}
+              key={index}
+            >
+              <div className="embla__slide__number">{index + 1}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export function Room() {
+  const navigate = useNavigate();
+  const { roomId = "" } = useParams<{
+    roomId: string;
+  }>();
+
+  const handleBackClick = () => {
+    navigate(-1);
+  };
+
   return (
     <Container
       sx={{
@@ -14,10 +69,21 @@ export function Room() {
         },
       }}
     >
-      <Stack marginTop={6} paddingBottom={4} gap={2}>
-        <Typography variant="body1" fontWeight={500}>
-          Recent sessions
-        </Typography>
+      {/* <Stack marginTop={6} paddingBottom={4} gap={2}> */}
+      <Stack paddingBottom={4} gap={2}>
+        <Stack gap={1} alignItems={"flex-start"}>
+          <Button
+            variant="text"
+            onClick={handleBackClick}
+            startIcon={<KeyboardBackspaceOutlinedIcon />}
+          >
+            Back
+          </Button>
+          <Typography variant="body1" fontWeight={500}>
+            Room ID: {roomId}
+          </Typography>
+        </Stack>
+        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
       </Stack>
     </Container>
   );
