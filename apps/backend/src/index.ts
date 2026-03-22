c0bc2e92cb40b4f5071fe7b7e8c900fdf5dcfa01
Reuben Mark Gorda <reuben.gorda@gmail.com>
2026-03-22 12:28:04 +0800

    remove type annotations

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,1 @@
-import { ExecutionContext, ExportedHandler } from "@cloudflare/workers-types";
-
-import app from "./app";
-import type { Env } from "./shared/config/env";
-
-export default {
-  fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return app.fetch(request, env, ctx);
-  },
-} satisfies ExportedHandler<Env>;
+export { default } from "./app";
