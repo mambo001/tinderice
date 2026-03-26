36949680a9fe35ca1c9e708aefb9d38572645e71
Reuben <reuben.gorda@gmail.com>
2026-03-26 14:25:46 +0800

    add better seed dishes data

--- apps/backend/src/application/support/poll-candidates.ts
+++ apps/backend/src/application/support/poll-candidates.ts
@@ -6,297 +6,272 @@ export interface PollCandidateDish {
 
 export const POLL_CANDIDATE_DISHES: readonly PollCandidateDish[] = [
   {
-    id: "fbf50bb3-ef40-4c4b-8373-243f14bd9f3e",
-    name: "Big Mac",
-    imageUrl:
-      "https://www.themealdb.com/images/media/meals/urzj1d1587670726.jpg",
-  },
-  {
-    id: "8064372f-109b-448f-922b-5964172a5f79",
-    name: "Shawarma",
-    imageUrl:
-      "https://www.themealdb.com/images/media/meals/kcv6hj1598733479.jpg",
-  },
-  {
-    id: "d2d4d3ed-f9c3-4557-a2c8-6eb8673d3aab",
-    name: "Lasagne",
-    imageUrl:
-      "https://www.themealdb.com/images/media/meals/wtsvxx1511296896.jpg",
-  },
-  {
-    id: "ae86acf2-5698-411e-9635-875503b4c456",
-    name: "Beef Rendang",
-    imageUrl:
-      "https://www.themealdb.com/images/media/meals/bc8v651619789840.jpg",
-  },
-  {
-    id: "766dd97f-f3dd-4e1b-b036-0de443760b9c",
-    name: "Pancakes",
-    imageUrl:
-      "https://www.themealdb.com/images/media/meals/rwuyqx1511383174.jpg",
-  },
-  {
-    id: "bf042f9a-0514-4e48-8bea-29600df0d6d1",
-    name: "Ratatouille",
-    imageUrl:
-      "https://www.themealdb.com/images/media/meals/wrpwuu1511786491.jpg",
-  },
-  {
-    id: "fd510de8-4637-48ca-922d-e4a15c57bfe7",
-    name: "Apple Frangipan Tart",
-    imageUrl:
-      "https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg",
-  },
-  {
-    id: "67d76685-bd4e-40da-a18a-cf714a9013a6",
-    name: "Pork Sinigang",
-    imageUrl:
-      "https://images.pexels.com/photos/7024389/pexels-photo-7024389.jpeg",
-  },
-  {
-    id: "6618df2b-78c8-4d6b-8668-886533cff35f",
-    name: "Pork Adobo With Boiled Egg",
-    imageUrl:
-      "https://images.pexels.com/photos/6896058/pexels-photo-6896058.jpeg",
-  },
-  {
-    id: "024f7b63-f2ab-47b5-b2f7-65c5403c358b",
-    name: "Pork Nilaga",
-    imageUrl:
-      "https://images.pexels.com/photos/6895912/pexels-photo-6895912.jpeg",
-  },
-  {
-    id: "082de3fb-68eb-4297-99f1-427c02b560a8",
-    name: "Fried Pork Liempo",
-    imageUrl:
-      "https://images.pexels.com/photos/8625452/pexels-photo-8625452.jpeg",
-  },
-  {
-    id: "5d0fcbfe-d7e0-448d-b349-9625738e1f45",
-    name: "Lumpia Shanghai",
-    imageUrl:
-      "https://images.pexels.com/photos/36151673/pexels-photo-36151673.jpeg",
-  },
-  {
-    id: "91a8a4f2-5bed-481c-9908-1b613c51b3e5",
-    name: "Chicken Inasal",
-    imageUrl:
-      "https://images.pexels.com/photos/5031938/pexels-photo-5031938.jpeg",
-  },
-  {
-    id: "4cd07c9d-fdbc-4203-bd9b-6f88f3d72cd0",
-    name: "Chicken Adobo",
-    imageUrl:
-      "https://images.pexels.com/photos/2611917/pexels-photo-2611917.jpeg",
-  },
-  {
-    id: "1dedd3df-67c5-4c69-9164-5ea972a44f35",
-    name: "Roasted Chicken Wings",
-    imageUrl:
-      "https://images.pexels.com/photos/2338407/pexels-photo-2338407.jpeg",
-  },
-  {
-    id: "8d0e1bed-842f-4241-9955-d94556dbbc3b",
-    name: "Roasted Chicken",
-    imageUrl:
-      "https://images.pexels.com/photos/616353/pexels-photo-616353.jpeg",
-  },
-  {
-    id: "82aaa309-0503-49c7-9164-4c4010b2a65e",
-    name: "Fried Chicken",
-    imageUrl:
-      "https://images.pexels.com/photos/5652260/pexels-photo-5652260.jpeg",
-  },
-  {
-    id: "abf20657-e119-470f-b41f-c22899481362",
-    name: "Chicken Biryani",
-    imageUrl:
-      "https://images.pexels.com/photos/36282643/pexels-photo-36282643.jpeg",
-  },
-  {
-    id: "c4826ae8-d316-4825-8328-dcf9b8198e1a",
-    name: "Chicken Curry",
-    imageUrl:
-      "https://images.pexels.com/photos/36202022/pexels-photo-36202022.jpeg",
-  },
-  {
-    id: "1662434f-124d-453a-b619-7d543339da82",
-    name: "Sweet & Sour Chicken",
-    imageUrl:
-      "https://images.pexels.com/photos/5848522/pexels-photo-5848522.jpeg",
-  },
-  {
-    id: "085ad428-88d8-459d-a3a6-9c710f2ea7be",
-    name: "Ginisang Munggo With Kalabasa",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2025/07/Ginisang-munggo-with-squash-in-a-pot.jpg",
-  },
-  {
-    id: "d7d39c8e-fa44-4dc8-b579-eadae2a22813",
-    name: "Oven Roasted Asparagus",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2025/02/oven-roasted-asparagus-1024x683.jpg",
-  },
-  {
-    id: "1eeb86ab-568f-49c7-b490-c1b69bc1f63b",
-    name: "Chopsuey",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2023/05/chop-suey-recipe.jpg",
-  },
-  {
-    id: "4a87d41d-1f23-458b-a481-292d4d3f6600",
-    name: "Utan Nga Langka",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2022/07/ginataang-langka-with-bago-leaves.jpg",
-  },
-  {
-    id: "4f92b583-66fd-43ed-8cf8-22810e811f85",
-    name: "Ginataang Kalabasa At Sitaw",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2021/05/ginataang-sitaw-recipe.jpg",
-  },
-  {
-    id: "adfff07e-6b1b-4c48-8386-b7a6b576a7d1",
-    name: "Ginisang Ampalaya With Egg",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2009/07/Ginisang-Ampalaya-with-Eggs.jpg",
-  },
-  {
-    id: "e84eee1c-81cf-456d-a38a-b3936c98e5be",
-    name: "Tortang Talong",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2021/07/Tortang-Talong-1024x1536.jpg",
-  },
-  {
-    id: "c31e438e-d66f-4f6a-8fdd-9fbdf3c1a1a7",
+    id: "125b77e8-15ec-4a3f-85dd-f98e6868f17f",
     name: "Adobong Kangkong",
     imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2010/07/adobong-kangkong-1.jpg",
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Adobong Kangkong.webp",
   },
   {
-    id: "4a5feb7b-8ac9-4a05-bc91-8bbcf1bcde7e",
-    name: "Pinakbet",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2019/01/Pinakbet-Tagalog-Recipe.jpg",
-  },
-  {
-    id: "d3af7834-059b-4195-a1c5-02e500eb3db1",
-    name: "Ginisang Sayote With Pork",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2019/04/Ginisang-Sayote.jpg",
-  },
-  {
-    id: "86780fec-90a2-40c7-80e0-bcddd1f4b5cc",
-    name: "Laing",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2018/12/Laing-na-Gabi-Recipe.jpg",
-  },
-  {
-    id: "4039a357-6b9d-4967-90b2-be7df99a1236",
-    name: "Ginisang Repolyo",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2019/01/ginisang-repolyo.jpg",
-  },
-  {
-    id: "cad6e939-6c48-4c38-93ac-660aa397fd2e",
-    name: "Paksiw Na Bangus",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2025/03/bangus-paksiw-recipe.jpg",
-  },
-  {
-    id: "c6399207-b0ac-42de-9074-41533562e2aa",
-    name: "Paksiw Na Isda Sa Gata",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2025/03/bangus-paksiw-recipe.jpg",
-  },
-  {
-    id: "1d66f6fe-cccf-47d8-a97f-c187fe67cb0c",
-    name: "Sweet & Sour Fish Fillet",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2025/03/sweet-and-sour-fish-fillet-recipe.jpg",
-  },
-  {
-    id: "087bf035-32be-4341-9b4c-48f823655649",
-    name: "Fish Sinigang",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2023/03/Fish-Sinigang.jpg",
-  },
-  {
-    id: "241750e4-fd65-4659-adc2-d70ab1500db0",
-    name: "Fish Escabeche",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2022/08/fish-escabeche-recipe.jpg",
-  },
-  {
-    id: "216f075e-5e10-4965-b1e6-99001bf83470",
-    name: "Fried Fish",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2010/02/Fried-Fish-Tilapia.jpg",
-  },
-  {
-    id: "2ae4cf27-2d88-4a95-8628-ec1dac4d66b9",
-    name: "Tuna Sisig",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2018/05/Tuna-Sisig.jpg",
-  },
-  {
-    id: "da6a4557-4151-4b95-9795-77b9af581343",
-    name: "Garlic Pepper Beef",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2025/09/Saucy-garlic-pepper-beef.jpg",
-  },
-  {
-    id: "cc9000fb-7a66-44e6-9c7c-063eb98a482d",
-    name: "Beef Caldereta",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2025/03/How-to-cook-Caldereta.jpg",
-  },
-  {
-    id: "14e2b0b8-b2b3-40b0-8da4-db4438405e05",
-    name: "Beef Nilaga",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2023/04/beef-nilaga-recipe-panlasang-pinoy.jpg",
-  },
-  {
-    id: "a02db9d8-334a-49a9-96b6-10e7ff2fe0f4",
-    name: "Beef Kare-Kare",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2023/04/ox-tail-kare-kare.jpg",
-  },
-  {
-    id: "921dcdbf-d6c3-4ea6-b4c3-4b65f043f2a2",
-    name: "Garlic Pepper Beef",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2022/11/garlic-pepper-beef-jpg.webp",
-  },
-  {
-    id: "657ed098-98c1-47d2-ab31-1d3fc412b27b",
-    name: "Beef Shank Soup",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2009/03/Beef-Shank-Soup-Recipe.jpg",
-  },
-  {
-    id: "ab3098c4-3996-41cd-aad9-5f1347d548ba",
-    name: "Beef Mechado",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2009/07/Easy-Beef-Mechado.jpg",
-  },
-  {
-    id: "3a59c5b7-3e65-42a9-9ea7-205be205ad81",
-    name: "Burger Steak",
-    imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2010/06/burger-steak-1.jpg",
-  },
-  {
-    id: "5fd490f9-3523-48fc-80aa-06c5f4d26bdd",
+    id: "df1fd240-5dd0-4e42-9f67-24ee6cda53fd",
     name: "Beef Asado",
     imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2020/09/beef-asado.jpg",
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Beef Asado.webp",
   },
   {
-    id: "f1d9619a-eea6-400b-9404-23bf2f1c59b9",
-    name: "Beef with Broccoli",
+    id: "b7924a80-c38f-476f-9051-38fd7708730e",
+    name: "Beef Caldereta",
     imageUrl:
-      "https://panlasangpinoy.com/wp-content/uploads/2020/04/saucy-beef-with-broccoli.jpg",
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Beef Caldereta.webp",
+  },
+  {
+    id: "4ab61e2d-e376-4afb-842f-a3ab67f77310",
+    name: "Beef Kare-Kare",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Beef Kare-Kare.webp",
+  },
+  {
+    id: "33b830c1-7b56-4d96-8357-8ac32a67bafb",
+    name: "Beef Mechado",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Beef Mechado.webp",
+  },
+  {
+    id: "431a7b74-602e-4624-bbf6-ce09ea607a3d",
+    name: "Beef Nilaga",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Beef Nilaga.webp",
+  },
+  {
+    id: "882fa234-7b48-4f55-b3f3-e67834c432fe",
+    name: "Beef With Broccoli",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Beef With Broccoli.webp",
+  },
+  {
+    id: "fb90e6ff-7352-4415-8ce0-f9b85edf284e",
+    name: "Burger Steak",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Burger Steak.webp",
+  },
+  {
+    id: "e129efad-52de-4078-abc9-4b463dbb014e",
+    name: "Caprese Salad",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Caprese Salad.webp",
+  },
+  {
+    id: "92675dfe-b221-42db-85b8-8a98ddc5f657",
+    name: "Chicken Biryani",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Chicken Biryani.webp",
+  },
+  {
+    id: "3444f43a-36ab-4c14-9955-e1c0995445ab",
+    name: "Chicken Curry",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Chicken Curry.webp",
+  },
+  {
+    id: "226c1bdc-8986-42b3-9af3-39101d4d43ec",
+    name: "Chicken Inasal",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Chicken Inasal.webp",
+  },
+  {
+    id: "eb131652-6f13-4ce3-a157-303e2052ac13",
+    name: "Chopsuey",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Chopsuey.webp",
+  },
+  {
+    id: "66ebb7ce-d10d-44c9-8c48-a0cffc1e94a4",
+    name: "Fish Escabeche",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Fish Escabeche.webp",
+  },
+  {
+    id: "536e1098-7c4a-4ca3-9d0a-2f0f9658fc4e",
+    name: "Fish Sinigang",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Fish Sinigang.webp",
+  },
+  {
+    id: "557954ec-4556-4195-9583-11c4a13aa9cf",
+    name: "Fried Chicken",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Fried Chicken.webp",
+  },
+  {
+    id: "3a74784d-f383-40ad-9249-dc3b70d4b340",
+    name: "Fried Fish",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Fried Fish.webp",
+  },
+  {
+    id: "c93ffacb-bbe0-4964-b4ac-b8d9c5fb906a",
+    name: "Garlic Pepper Beef",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Garlic Pepper Beef.webp",
+  },
+  {
+    id: "0593a428-7e53-41fd-ba3d-4a43474c36f2",
+    name: "Ginataang Kalabasa At Sitaw",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Ginataang Kalabasa At Sitaw.webp",
+  },
+  {
+    id: "0492e1be-34b6-4052-8e67-d155f0dd0c46",
+    name: "Ginisang Ampalaya With Egg",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Ginisang Ampalaya With Egg.webp",
+  },
+  {
+    id: "57b14dee-38c9-4d94-8a3a-a7dbd164b5e7",
+    name: "Ginisang Munggo With Kalabasa",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Ginisang Munggo With Kalabasa.webp",
+  },
+  {
+    id: "93bc8802-67d2-4d0c-be8a-29fcd415387c",
+    name: "Ginisang Repolyo",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Ginisang Repolyo.webp",
+  },
+  {
+    id: "6b193187-683b-405d-ac34-0ca843e575e5",
+    name: "Ginisang Sayote With Pork",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Ginisang Sayote With Pork.webp",
+  },
+  {
+    id: "82de60ad-e477-4e79-aac3-3642a2f74e59",
+    name: "Greek Salad",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Greek Salad.webp",
+  },
+  {
+    id: "df713efa-b2a4-44f6-855d-c31e1ac78efe",
+    name: "Grilled Pork Belly",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Grilled Pork Belly.webp",
+  },
+  {
+    id: "83e36705-53b5-4359-98d4-4ca4ab053de2",
+    name: "Honey Balsamic Glazed Chicken",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Honey Balsamic Glazed Chicken.webp",
+  },
+  {
+    id: "3caef6cd-b6ac-4532-bda2-cea11cf70c5f",
+    name: "Laing",
+    imageUrl: "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Laing.webp",
+  },
+  {
+    id: "0918f627-e4e0-42cc-805a-9ced2b959495",
+    name: "Lamb Tagine",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Lamb Tagine.webp",
+  },
+  {
+    id: "07357fb1-4420-42de-88d4-09f7ad20a0cd",
+    name: "Oven Roasted Asparagus",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Oven Roasted Asparagus.webp",
+  },
+  {
+    id: "5bc7e052-0a87-481e-9a45-8b1f85634640",
+    name: "Paksiw Na Bangus",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Paksiw Na Bangus.webp",
+  },
+  {
+    id: "e29bb54a-1ffd-41d2-830a-8af45b11c5ba",
+    name: "Pinakbet",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Pinakbet.webp",
+  },
+  {
+    id: "f4a626c9-ab5a-4de1-8a3d-856454a17b46",
+    name: "Pork Adobo With Boiled Egg",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Pork Adobo With Boiled Egg.webp",
+  },
+  {
+    id: "a1305d7f-9cba-4394-b219-75d97b667624",
+    name: "Pork Lumpia",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Pork Lumpia.webp",
+  },
+  {
+    id: "ec02000c-75d2-402c-8a2f-a0fa1b6ca7dc",
+    name: "Pork Nilaga",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Pork Nilaga.webp",
+  },
+  {
+    id: "9a10ac2f-f171-420b-88f5-852ca89cb4ee",
+    name: "Pork Sinigang",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Pork Sinigang.webp",
+  },
+  {
+    id: "4dccc1fa-6bce-4fd8-be3e-d638becc00bb",
+    name: "Roasted Chicken Wings",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Roasted Chicken Wings.webp",
+  },
+  {
+    id: "e3837d3f-ed0e-4978-93a0-f2f81eef13f6",
+    name: "Roasted Whole Chicken",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Roasted Whole Chicken.webp",
+  },
+  {
+    id: "c76ee2ed-2566-46e9-b63e-913a1ac5a482",
+    name: "Salmon & Asparagus Salad",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Salmon & Asparagus Salad.webp",
+  },
+  {
+    id: "881d3b01-1e0d-4d4c-b41b-3cf737f628bf",
+    name: "Shakshuka",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Shakshuka.webp",
+  },
+  {
+    id: "ad198d4a-3c0e-4faa-81e0-d1e3398fbec5",
+    name: "Shrimp & Chorizo Paella",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Shrimp & Chorizo Paella.webp",
+  },
+  {
+    id: "55aabe86-b576-4d02-97cb-5c16a22a2786",
+    name: "Sweet & Sour Chicken",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Sweet & Sour Chicken.webp",
+  },
+  {
+    id: "bc30e902-5ec4-43f8-84e4-218f851bb54b",
+    name: "Sweet & Sour Fish Fillet",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Sweet & Sour Fish Fillet.webp",
+  },
+  {
+    id: "b695ade6-446b-4392-bc95-be5db93afc7a",
+    name: "Tortang Talong",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Tortang Talong.webp",
+  },
+  {
+    id: "a75deac5-2bbd-4026-8406-ff82e57a5509",
+    name: "Tuna Sisig",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Tuna Sisig.webp",
+  },
+  {
+    id: "a2305c88-36ed-49d1-93f6-968351df07bc",
+    name: "Utan Nga Langka",
+    imageUrl:
+      "https://pub-8d9389364ad34387916aaf7ae4b70100.r2.dev/Utan Nga Langka.webp",
   },
 ];
